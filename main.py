d5468db3a5df91f9420b8e74b136c3d574bba398
Kazuo Nishiyama <nopeet@gmail.com>
2024-07-15 23:42:46 +0900

    Change num_epoch to 20

--- main.py
+++ main.py
@@ -426,8 +426,8 @@ def main():
     model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device, non_blocking=True)
 
     # optimizer / criterion
-    # num_epoch = 20
-    num_epoch = 1
+    num_epoch = 20
+    # num_epoch = 1
     criterion = nn.CrossEntropyLoss()
     # criterion = nn.KLDivLoss()
     lr = 0.001
